6ce57d1b12a6136767dfd7433bbc375ff81c22dc
Charan <32680609+charan40@users.noreply.github.com>
2022-10-16 02:24:11 +0700

    Add CSS version 1 - Z
    Add some of CSS from Bootstrap. In case of tailwind I'm not understand yet...

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/**/*.html',
+    './views/register.html',
+    './views/**/*.ejs',
+    './views/login.ejs',
+    "./src/**/*.{html,js}",
+    "./src/**/*.{html,ejs}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

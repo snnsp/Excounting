00e94871819fa7d46711fbfbc7645f31042f9d3f
Chaiyasit Apirakkitikul <tempest545454@gmail.com>
2022-11-23 09:26:11 +0700

    Edit some config for ProEn hosting

--- app.js
+++ app.js
@@ -41,3 +41,13 @@ app.post('/AddBalance', TokenVerity, AccountController.postAddBalance)
 
 
 module.exports = app
+
+const http = require('http')
+const server = http.createServer(app)
+
+const {API_PORT} = process.env
+const port = process.env.PORT
+
+server.listen(port, ()=>{
+    console.log(`Server running on PORT ${port}`)
+})
